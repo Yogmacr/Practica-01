c03eebfc6385c06b6a81181ae5cf0bfb9bde645a
Yogmacr <manuel.castaneda1189@alumnos.udg.mx>
2024-08-19 23:07:04 -0600

    first commit

---
+++ MyApp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class MyApp extends Component {
+  render() {
+    return (
+      <View>
+        <Text style={{ color: 'gold', fontSize: '70', fontStyle: 'italic'}}>
+          HOLA SOY MANUEL
+        </Text>
+      </View>
+    )
+  }
+}
